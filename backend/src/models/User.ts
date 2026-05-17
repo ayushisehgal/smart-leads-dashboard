10b18b86fe6a4fb84f6ac48155e6b5355f9f319e
ayushisehgal <sehgalayushi488@gmail.com>
2026-05-18 04:32:44 +0530

    fix: fix User model typescript error for production build

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema, CallbackError } from 'mongoose';
 import bcrypt from 'bcryptjs';
 import { IUser } from '../types';
 
@@ -13,10 +13,16 @@ const UserSchema = new Schema<IUser>(
 );
 
 UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(12);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err as CallbackError);
+  }
 });
 
 UserSchema.methods.comparePassword = async function (
